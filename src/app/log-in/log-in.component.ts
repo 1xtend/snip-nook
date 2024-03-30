d3109386fd4fb76174bf5150a45d7da5ee6ff0ee
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-03-30 15:50:32 +0100

    refactor(layout): switch from p-card to custom styles

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthForm } from '../shared/models/auth.interface';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
   imports: [
-    CardModule,
     InputTextModule,
     PasswordModule,
     ButtonModule,
     RouterLink,
+    ReactiveFormsModule,
   ],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss',
 })
-export class LogInComponent {}
+export class LogInComponent implements OnInit {
+  form!: FormGroup<AuthForm>;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group<AuthForm>({
+      email: this.fb.control('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.email],
+      }),
+      password: this.fb.control('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+        ],
+      }),
+    });
+  }
+}
