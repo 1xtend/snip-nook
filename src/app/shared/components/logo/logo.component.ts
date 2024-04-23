e442f6bcc123ede91cb1fc84690e0fe1926d98dd
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-04-23 13:08:55 +0200

    style: logo and header components, add create snippet route to header

--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-logo',
   standalone: true,
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink],
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
