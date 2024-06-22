8c807da5bff96f993bbf75872f4a11257361cb5b
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-06-22 16:22:25 +0200

    feat: display socials in profile-settings component

--- src/app/features/settings/profile-settings/profile-settings.component.ts
+++ src/app/features/settings/profile-settings/profile-settings.component.ts
@@ -27,9 +27,10 @@ import {
 } from '@angular/forms';
 import { DialogType } from '@shared/models/dialog.type';
 import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { InputGroupModule } from 'primeng/inputgroup';
 import { UsernameDialogComponent } from '@shared/components/username-dialog/username-dialog.component';
 import { DescriptionDialogComponent } from '@shared/components/description-dialog/description-dialog.component';
-import { IProfileForm } from '@shared/models/form.types';
+import { IProfileForm, ISocialFormGroup } from '@shared/models/form.types';
 import { ISocial } from '@shared/models/user.interface';
 import { ChipComponent } from '@shared/components/chip/chip.component';
 import { SocialsDialogComponent } from '@shared/components/socials-dialog/socials-dialog.component';
@@ -45,6 +46,7 @@ import { SocialsDialogComponent } from '@shared/components/socials-dialog/social
     FileUploadModule,
     CalendarModule,
     ChipComponent,
+    InputGroupModule,
   ],
   templateUrl: './profile-settings.component.html',
   styleUrl: './profile-settings.component.scss',
@@ -70,9 +72,10 @@ export class ProfileSettingsComponent implements OnInit {
       nonNullable: true,
       validators: [Validators.required],
     }),
-    socials: this.fb.array<FormControl<ISocial>>([], {
-      validators: [Validators.required],
-    }),
+    socials: this.fb.array<FormGroup<ISocialFormGroup>>(
+      this.getSocialsArray(),
+      { validators: [Validators.required, Validators.maxLength(5)] },
+    ),
   });
 
   maxDate = new Date();
@@ -127,17 +130,26 @@ export class ProfileSettingsComponent implements OnInit {
       )
       .subscribe({
         next: (user) => {
-          let socials = this.fb.array<FormControl<ISocial>>([]);
+          // let socials = this.fb.array<FormControl<ISocial | null>>([]);
+
+          // if (user?.socials) {
+          //   socials = this.fb.array(
+          //     user.socials.map((item) => {
+          //       return this.fb.control<ISocial | null>(item, {
+          //         nonNullable: true,
+          //       });
+          //     }),
+          //   );
+          // }
+
+          // this.form.setControl('socials', socials);
 
           if (user?.socials) {
-            socials = this.fb.array(
-              user.socials.map((item) => {
-                return this.fb.control<ISocial>(item, { nonNullable: true });
-              }),
+            this.form.setControl(
+              'socials',
+              this.setSocialsControl(user.socials),
             );
           }
-
-          this.form.setControl('socials', socials);
 
           this.form.patchValue({
             description: user?.description || '',
@@ -146,11 +158,35 @@ export class ProfileSettingsComponent implements OnInit {
 
           this.pending.set(false);
           this.form.enable();
+
+          console.log(this.form.getRawValue());
         },
         error: () => {
           this.pending.set(false);
           this.form.enable();
         },
       });
+  }
+
+  private getSocialsArray(socials?: ISocial[]): FormGroup<ISocialFormGroup>[] {
+    const groupsArray: FormGroup<ISocialFormGroup>[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      groupsArray.push(this.createGroup(socials?.[i]));
+    }
+
+    return groupsArray;
+  }
+
+  private setSocialsControl(socials: ISocial[]) {
+    return this.fb.array(this.getSocialsArray(socials));
+  }
+
+  private createGroup(social?: ISocial) {
+    return this.fb.group<ISocialFormGroup>({
+      icon: this.fb.control(social?.icon || '', { nonNullable: true }),
+      name: this.fb.control(social?.name || '', { nonNullable: true }),
+      link: this.fb.control(social?.link || '', { nonNullable: true }),
+    });
   }
 }
--- src/app/shared/models/form.types.ts
+++ src/app/shared/models/form.types.ts
@@ -1,12 +1,18 @@
-import { FormArray, FormControl } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ISocial } from './user.interface';
 
 export interface IDescriptionForm {
   description: FormControl<string>;
 }
 
+export interface ISocialFormGroup {
+  name: FormControl<string>;
+  icon: FormControl<string>;
+  link: FormControl<string>;
+}
+
 export interface IProfileForm {
   description: FormControl<string>;
-  socials: FormArray<FormControl<ISocial>>;
+  socials: FormArray<FormGroup<ISocialFormGroup>>;
   birthday: FormControl<string>;
 }
